36497917f87a880f8c8b035a69be11b0e166d395
kjwallac <kristinajwallace@gmail.com>
2021-01-26 22:53:18 -0800

    added render of calendar days through DOM, all days link up to correct weekdays per month

--- monthnav.js
+++ monthnav.js
@@ -1,58 +1,85 @@
 const date = new Date();
 
-
-
-const monthDays = document.getElementById('days');
-
-const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-
-const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
-
-const firstDayIndex = date.getDay();
-
-const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
-
-console.log(lastDayIndex);
-
 const renderCalendar = () => {
+  date.setDate(1);
+
+  const monthDays = document.querySelector(".days");
+
+  const lastDay = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDate();
+
+  const prevLastDay = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    0
+  ).getDate();
+
+  const firstDayIndex = date.getDay();
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
+  const nextDays = 7 - lastDayIndex - 1;
   const months = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
-  document.getElementById('monthName').innerHTML = months[date.getMonth()];
+  document.getElementById("monthName").innerHTML = months[date.getMonth()];
 
-  document.getElementById('year').innerHTML = date.getFullYear();
+  document.getElementById("year").innerHTML = date.getFullYear();
 
-  let days = '';
+  let days = "";
 
-  for(let x = firstDayIndex; x > 0; x--){
-      days += `<span class='prev-date'>${prevLastDay - x + 1}</span>`;
+  for (let x = firstDayIndex; x > 0; x--) {
+    days += `<button class='button prev-date'>${prevLastDay - x + 1}</button>`;
   }
 
-  for (let i = 1;i <= lastDay; i++) {
-      days += `<span>${i}</span>`;
-      monthDays.innerHTML = days;
-   }
+  for (let i = 1; i <= lastDay; i++) {
+    days += `<button class='button'>${i}</button>`;
+    monthDays.innerHTML = days;
+  }
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<button class ='button next date'>${j}</button>`;
+    monthDays.innerHTML = days;
+  }
 };
 
-document.querySelector('.prev').addEventListener('click', () => {
+document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
   renderCalendar();
 });
 
-document.querySelector('.next').addEventListener('click', () => {
+document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
 
 renderCalendar();
+
+// const startWeek = moment().startOf('month').week();
+// const endWeek = moment().endOf('month').week();
+
+// // let calendar = []
+// // for(var week = startWeek; week<endWeek;week++){
+// //   calendar.push({
+// //     week:week,
+// //     days:Array(7).fill(0).map((n, i) => moment().week(week).startOf('week').clone().add(n + i, 'day'))
+// //   })
+// // }
