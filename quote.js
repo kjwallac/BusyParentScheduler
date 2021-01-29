172d8dcae92b84c8fd1dd60f70e456ac0fe7023c
kjwallac <kristinajwallace@gmail.com>
2021-01-28 22:30:39 -0800

    javascript for quote API

---
+++ quote.js
@@ -0,0 +1,27 @@
+//add div and script to html file to render quote:
+// <div id="quoteEl"></div>
+// <script src="quote.js"></script>
+
+async function getQuote() {
+  const URL = "https://favqs.com/api/quotes/?filter=parenting&type=tag";
+  const response = await fetch(URL, {
+    method: "GET",
+    headers: {
+      Authorization: 'Token token="31f810b95d45ce0a98f579b5452e635d"',
+    },
+  });
+  if(response.ok){
+      const quoteJSON = await response.json();
+      const index = Math.floor(quoteJSON.quotes.length * Math.random());
+      const quote = quoteJSON.quotes[index];
+      return `${quote.body} - ${quote.author}`;
+  }
+  return 'no quote';
+}
+
+async function populateQuote(quoteElID) {
+    const element = document.getElementById(quoteElID);
+    element.innerHTML = await getQuote();
+}
+
+populateQuote('quoteEl');
